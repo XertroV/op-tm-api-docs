fdfc3b982955b12bcae6ec65ba2749ab06b5eb57
Max Kaye <m@xk.io>
2023-10-17 18:50:38 +1100

    update op json

--- src/components/TabLabel.vue
+++ src/components/TabLabel.vue
@@ -2,8 +2,8 @@
     <div class="shrink rounded-t border hover:bg-stone-600 active:bg-stone-800 select-none flex flex-row justify-between"
         :to="`/${name}`" :class="isFocused ? 'bg-stone-700' : ''"
         :title="name"
-        @click.middle="emitter.emit('closeTab', name)
-        ">
+        @click.middle.prevent.stop="emitter.emit('closeTab', name)"
+        >
         <router-link :to="`/${name}`" class="tab-label truncate py-1.5 px-2 inline-block h-full w-full">{{ name }}</router-link>
         <div v-if="canClose" class="px-2 py-1.5 inline-block hover:text-white hover:cursor-pointer" @click="emitter.emit('closeTab', name)">❎</div>
     </div>
