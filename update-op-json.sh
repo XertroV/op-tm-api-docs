9b178233601ebbb98c1062a9c984ac7c6ee3c912
Max Kaye <m@xk.io>
2023-11-29 11:46:12 +1100

    update docs

---
+++ update-op-json.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+MPVERSION=$(cat $HOME/OpenplanetNext/OpenplanetNext.json_with_offsets.json | jq .mp | cut -d ' ' -f 1 | tr -d '"')
+DESTFILE=op-$MPVERSION.json
+
+echo "VER: $MPVERSION"
+echo "DEST: $DESTFILE"
+
+cp $HOME/OpenplanetNext/OpenplanetNext.json_with_offsets.json $DESTFILE
+cp $DESTFILE src/OpenplanetNext.json
+
+
